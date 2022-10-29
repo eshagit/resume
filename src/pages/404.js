84553e08769e14fb1d36e9cddad37c6989cd733e
Esha Mohideen <esha.mohideen@siemens.com>
2022-10-30 00:47:28 +0530

    removed default loading

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 
+const browser = typeof window !== "undefined" && window;
 
 const NotFoundPage =()=>{
     return(
+        browser && (
         <h2>error page</h2>
     )
+    );
         
     }
 
